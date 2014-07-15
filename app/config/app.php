7b2cbd912d3c7f8ea85b6757fc637637bc7ae494
Ethan Wortzman <ethanwortzman@g.harvard.edu>
2014-07-15 00:54:27 -0400

    removed lorem-ipsum plugin from composer.json and it's associated alias

--- app/config/app.php
+++ app/config/app.php
@@ -188,9 +188,7 @@ return array(
 		'URL'             => 'Illuminate\Support\Facades\URL',
 		'Validator'       => 'Illuminate\Support\Facades\Validator',
 		'View'            => 'Illuminate\Support\Facades\View',
-		'Faker'           => 'Faker\Factory',
-		'Faker_Provider'  => 'Faker\Provider',
-		'Lorem'           => 'Badcow\LoremIpsum\Generator'
+		'Faker'           => 'Faker\Factory'
 	),
 
 );
